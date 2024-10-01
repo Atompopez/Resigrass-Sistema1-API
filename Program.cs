39256b37bf8e0bdbe958215d0d864f0eaf82cf0e
Johan <johanandrey0210@gmail.com>
2024-10-01 01:38:06 -0500

    update dockerfile

--- Program.cs
+++ Program.cs
@@ -1,27 +1,24 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
-using ResiGrass_API.Logic; // Asegúrate de que este espacio de nombres sea correcto
+using ResiGrass_API.Logic;
 using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Inicializa la conexión utilizando el espacio de nombres correcto
 ResiGrass_API.Logic.Globals.Initialize(builder.Configuration);
 
-// Asegúrate de que la clase DbQuery esté en el espacio de nombres correcto
 builder.Services.AddSingleton<DbQuery>(new DbQuery(ResiGrass_API.Logic.Globals.ConnectionString));
 
-// Add services to the container.
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
-    c.SwaggerDoc("v1", new() { Title = "API_SUIP_Link", Version = "v1" });
+    c.SwaggerDoc("v1", new() { Title = "ResiGrass API", Version = "v1" });
 
     c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
     {
-        Description = "JWT Authorization",
+        Description = "Ingrese el token JWT en este formato: Bearer {token}",
         Name = "Authorization",
         In = ParameterLocation.Header,
         Type = SecuritySchemeType.Http,
@@ -39,7 +36,7 @@ builder.Services.AddSwaggerGen(c =>
                     Id = "Bearer"
                 }
             },
-            new string[] { }
+            new string[] {}
         }
     });
 });
@@ -70,8 +67,10 @@ if (app.Environment.IsDevelopment())
     });
 }
 
-app.UseHttpsRedirection();
+app.UseAuthentication();
 app.UseAuthorization();
+
+app.UseHttpsRedirection();
 app.MapControllers();
 app.Urls.Add("http://0.0.0.0:5023");
 app.Run();
